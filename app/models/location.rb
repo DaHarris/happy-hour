30d434962cc854e3843c66a063c631fb8e01fcd7
Daniel Harris <bobth3bum@gmail.com>
2015-01-29 13:04:53 -0700

    added conditional for empty hrating string

--- app/models/location.rb
+++ app/models/location.rb
@@ -8,14 +8,16 @@ class Location < ActiveRecord::Base
   #converts the rating to a string for referencing icons
   #in application.js for the google maps marker
   def convert()
-    if hrating == 0;
-      'icons.unrated.icon'
-    elsif hrating < 3.5
-      'icons.bad.icon'
-    elsif hrating < 4.25
-      'icons.average.icon'
-    else
-      'icons.great.icon'
+    if hrating != nil
+      if hrating == 0
+        'icons.unrated.icon'
+      elsif hrating < 3.5
+        'icons.bad.icon'
+      elsif hrating < 4.25
+        'icons.average.icon'
+      else
+        'icons.great.icon'
+      end
     end
   end
 
